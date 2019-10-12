7a59e49bfbb758bedeb7841fe406293242fa4c03
nancylee713 <srlee713@gmail.com>
2019-10-12 11:58:00 -0600

    Add friendship model and test
    Co-Authored-By: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-Authored-By: Nathan Thomas <47466067+nathangthomas@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_162359) do
+ActiveRecord::Schema.define(version: 2019_10_12_164657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "identities", force: :cascade do |t|
     t.string "provider"
     t.integer "uid"
     t.bigint "user_id"
-    t.string "username"
+    t.string "user_name"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
