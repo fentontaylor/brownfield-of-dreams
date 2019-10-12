60a3b5927100afee4dacfb50cd33a3a7fe41ea8a
Fenton Taylor <fentontaylor@gmail.com>
2019-10-12 15:42:03 -0600

    Add test for removing friends from friends list
    Co-Authored-By: Nancy Lee <24424825+nancylee713@users.noreply.github.com>
    Co-Authored-By: Nathan Thomas <7466067+nathangthomas@users.noreply.github.com>

--- spec/features/user/user_can_add_and_remove_friends_spec.rb
+++ spec/features/user/user_can_add_and_remove_friends_spec.rb
@@ -77,12 +77,33 @@ describe 'A registered user' do
     end
   end
 
-  it "can click a link to delete friend" do
+  it "can click a link to remove a friend" do
+    user = @github_follower_user
+    github_user_name = @github_follower_user_identity.user_name
+
     within '.github-followers' do
-      within "#follower-#{@github_follower_user_identity.user_name}" do
-        expect(page).to_not have_button("Add as Friend")
+      within "#follower-#{github_user_name}" do
+        click_button("Add as Friend")
+      end
+    end
+
+    within '.friends-list' do
+      within "#friend-#{user.id}" do
+        expect(page).to have_button('Remove Friend')
+        click_button('Remove Friend')
+      end
+    end
+
+    within '.friends-list' do
+      expect(page).to_not have_css("#friend-#{user.id}")
+    end
+    
+    expect(page).to have_content("You removed #{user.first_name} #{user.last_name} from your friends list.")
+
+    within '.github-followers' do
+      within "#follower-#{github_user_name}" do
+        expect(page).to have_button('Add as Friend')
       end
     end
   end
-
 end
