348bee59775c5563ad883959a0a99cdb63699743
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-10-13 15:07:04 -0600

    Add Video.new to current_video method in tutorial_facade

--- app/facades/tutorial_facade.rb
+++ app/facades/tutorial_facade.rb
@@ -5,7 +5,10 @@ class TutorialFacade < SimpleDelegator
   end
 
   def current_video
-    if @video_id
+    if videos == []
+      videos << Video.new(title: "This tutorial has no videos at this time.", description: "Sorry, there are no videos for this tutorial at this time. We are working on it, please check back soon.", video_id: "0", thumbnail: "https://giphy.com/gifs/cjzkCDL3jZTZB6ki1B/html5", tutorial_id: self.id, position: 1)
+
+    elsif @video_id
       videos.find(@video_id)
     else
       videos.first
--- spec/features/user/tutorial_show_page_bug_spec.rb
+++ spec/features/user/tutorial_show_page_bug_spec.rb
@@ -1,3 +1,5 @@
+#This test is solely for the purpose of hitting my debugger to pry into this feature.
+
 require 'rails_helper'
 
 describe 'As a registered user' do
@@ -7,16 +9,18 @@ describe 'As a registered user' do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-    Tutorial.create!({title: 'Sample Tutorial', description: 'this is a test description'})
+    Tutorial.create!(title: 'Sample Tutorial', description: 'this is a test description')
   end
-  
+
   # If a tutorial doesn't have any videos and we visit its show page an error occurs when trying to call current_video.title since current_video is nil.
   it 'when i visit a toutorial show page i can confirm there is an error when trying to visit current_video.title when a tutorial has no videos' do
+
     visit root_path
     click_on('Sample Tutorial')
     #confirmed that current_video is nill when current_video.video is called
 
     #added conditional to check for current_video != nil before rendering the rest of the page. If it is nil it returns a message letting user know ther are no videos for the tutorial yet.
+
     expect(page).to have_content("This tutorial has no videos at this time.")
   end
 end
