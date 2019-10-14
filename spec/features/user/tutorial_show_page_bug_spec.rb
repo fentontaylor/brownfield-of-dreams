0cbdcb533028ff713cfb57845d5a45ef630d0f88
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-10-14 15:05:52 -0600

    Refactor tutorial_show_page_bug_spec

--- spec/features/user/tutorial_show_page_bug_spec.rb
+++ spec/features/user/tutorial_show_page_bug_spec.rb
@@ -1,5 +1,3 @@
-#This test is solely for the purpose of hitting my debugger to pry into this feature.
-
 require 'rails_helper'
 
 describe 'As a registered user' do
@@ -12,17 +10,10 @@ describe 'As a registered user' do
     Tutorial.create!(title: 'Sample Tutorial', description: 'this is a test description')
   end
 
-  # If a tutorial doesn't have any videos and we visit its show page an error occurs when trying to call current_video.title since current_video is nil.
-  it 'when i visit a toutorial show page i can confirm there is an error when trying to visit current_video.title when a tutorial has no videos' do
+  it 'when i visit a tutorial that has no videos I am directed to a default page notifying me that there are no videos for the given tutorial' do
 
     visit root_path
     click_on('Sample Tutorial')
-    
-    save_and_open_page
-
-    #confirmed that current_video is nill when current_video.video is called
-
-    #added conditional to check for current_video != nil before rendering the rest of the page. If it is nil it returns a message letting user know ther are no videos for the tutorial yet.
 
     expect(page).to have_content("This tutorial has no videos at this time.")
   end
