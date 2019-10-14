11a126a0f7f6f977038c1d05b1f1098b049631a3
nancylee713 <srlee713@gmail.com>
2019-10-13 21:58:01 -0600

    Add a sad path testing for cases when video.position is nil
    Co-Authored-By: Nathan Thomas <47466067+nathangthomas@users.noreply.github.com>
    Co-Authored-By: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>

--- spec/features/vistors/vistor_sees_a_video_show_spec.rb
+++ spec/features/vistors/vistor_sees_a_video_show_spec.rb
@@ -13,4 +13,15 @@ describe 'visitor sees a video show' do
     expect(page).to have_content(video.title)
     expect(page).to have_content(tutorial.title)
   end
+
+  it 'sees an error message  if there\'s a video associated with the tutorial that doesn\'t have a position set' do
+    tutorial = create(:tutorial)
+    video_1 = create(:video, tutorial_id: tutorial.id, position: nil)
+    video_2 = create(:video, tutorial_id: tutorial.id)
+
+    visit '/'
+
+    click_on tutorial.title
+    expect(page).to have_content("The page you were looking for doesn't exist.")
+  end
 end
