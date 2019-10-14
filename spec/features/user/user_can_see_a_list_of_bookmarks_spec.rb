b426191368eaab02ff3a8bb3d62640327e199361
nancylee713 <srlee713@gmail.com>
2019-10-14 17:07:58 -0600

    Add a spec test for showing bookmarked videos on user dashboard
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Nathan Thomas <47466067+nathangthomas@users.noreply.github.com>

---
+++ spec/features/user/user_can_see_a_list_of_bookmarks_spec.rb
@@ -0,0 +1,52 @@
+# And they should be organized by which tutorial they are a part of
+# And the videos should be ordered by their position
+
+require 'rails_helper'
+
+describe 'As a registered user' do
+  it 'can see list of all bookmarked segments' do
+
+    stub_default_github_info
+
+    user = create(:user, token: ENV['GITHUB_API_KEY'])
+    tutorial_1 = create(:tutorial, title: "How to Tie Your Shoes")
+    video_1 = create(:video, title: "The Bunny Ears Technique", tutorial: tutorial_1, position: 1)
+    video_2 = create(:video, title: "Tying a Basic Knot", tutorial: tutorial_1, position: 2)
+
+    tutorial_2 = create(:tutorial, title: "How to code in Ruby")
+    video_3 = create(:video, title: "How to use SimpleCov", tutorial: tutorial_2, position: 1)
+
+    bookmarked_video_1 = create(:user_video, user: user, video: video_1)
+    bookmarked_video_2 = create(:user_video, user: user, video: video_2)
+    bookmarked_video_3 = create(:user_video, user: user, video: video_3)
+
+    visit login_path
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_on 'Log In'
+
+    within '.bookmarked' do
+      within "#bookmarked-tutorial-#{tutorial_1.id}" do
+        expect(page).to have_content(tutorial_1.title)
+      end
+
+      within "#bookmarked-video-#{video_1.id}" do
+        expect(page).to have_content(video_1.title)
+      end
+
+      within "#bookmarked-video-#{video_2.id}" do
+        expect(page).to have_content(video_2.title)
+      end
+
+      within "#bookmarked-tutorial-#{tutorial_2.id}" do
+        expect(page).to have_content(tutorial_2.title)
+      end
+
+      within "#bookmarked-video-#{video_3.id}" do
+        expect(page).to have_content(video_3.title)
+      end
+    end
+  end
+end
