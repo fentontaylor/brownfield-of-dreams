0ecdb8f00d473ffdafa0ce0d4e608f4b17ac0699
nancylee713 <srlee713@gmail.com>
2019-10-15 11:41:46 -0600

    Refactor create action within InviteController
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Nathan Thomas <47466067+nathangthomas@users.noreply.github.com>

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -3,19 +3,14 @@ class InviteController < ApplicationController
   end
 
   def create
-    handle = params["GitHub Handle"]
-    service = GithubService.new(current_user)
-    data = service.get_email(handle)
-    email = data[:email]
-    real_name = data[:name]
+    facade = InviteFacade.new(params["GitHub Handle"], current_user)
 
-    if data[:message] == 'Not Found'
-      flash[:error] = "Sorry, #{handle} is not a valid GitHub handle."
-    elsif email.nil?
-      flash[:error] = "Sorry, #{handle} does not have a public email."
+    if facade.message == 'Not Found'
+      flash[:error] = "Sorry, #{facade.handle} is not a valid GitHub handle."
+    elsif facade.email.nil?
+      flash[:error] = "Sorry, #{facade.handle} does not have a public email."
     else
-      inviter = current_user.first_name + ' ' + current_user.last_name
-      InvitationMailer.invite(real_name, email, inviter).deliver_now
+      facade.send_invite
       flash[:success] = "Successfully sent invite!"
     end
 
