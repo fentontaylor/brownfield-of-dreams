209467cbc87264ae0d853acbc3380a25089b20f3
nancylee713 <srlee713@gmail.com>
2019-10-15 11:41:46 -0600

    Add get_email method to GithubService along with passing test
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Nathan Thomas <47466067+nathangthomas@users.noreply.github.com>

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -1,21 +1,69 @@
 require 'rails_helper'
 
 describe "GithubService" do
-  it "returns parsed repo data" do
+  before :each do
     VCR.turn_off!
     nancy = User.create!(email: 'nancy@example.com', first_name: 'Nancy', last_name: 'Lee', password:  "password", role: :default, token: ENV['GITHUB_API_KEY_NL'])
-    
-    nancys_repos = File.open('./fixtures/nancys_repos.json')
-    stub_request(:get, 'https://api.github.com/user/repos?affiliation=owner')
-      .to_return(status: 200, body: nancys_repos)
 
-    service = GithubService.new(nancy)
+    stub_github_info("nancys")
 
-    github_repos = service.get_repos
+    @service = GithubService.new(nancy)
+  end
+
+  it "returns parsed repo data" do
+    github_repos = @service.get_repos
 
     expect(github_repos).to be_an Array
 
     expect(github_repos.first).to have_key(:name)
     expect(github_repos.first).to have_key(:html_url)
+  end
+
+  it "returns parsed follower data" do
+    github_followers = @service.get_followers
+
+    expect(github_followers).to be_an Array
+
+    expect(github_followers.first).to have_key(:login)
+    expect(github_followers.first).to have_key(:html_url)
+  end
+
+  it "returns parsed following data" do
+    github_following = @service.get_following
+
+    expect(github_following).to be_an Array
+
+    expect(github_following.first).to have_key(:login)
+    expect(github_following.first).to have_key(:html_url)
+  end
+end
+
+describe 'Email invite' do
+  it "returns public email" do
+    VCR.turn_off!
+    WebMock.allow_net_connect!
+
+    user = create(:user)
+    service = GithubService.new(user)
+
+    valid_handle = 'fentontaylor'
+    data_1 = service.get_email(valid_handle)
+
+    expect(data_1[:email]).to eq('fentontaylor@gmail.com')
+
+    valid_handle_no_email = 'nancylee713'
+    data_2 = service.get_email(valid_handle_no_email)
+
+    expect(data_2[:email]).to eq(nil)
+
+    invalid_handle = 'asdfkljd;'
+    no_data =  service.get_email(invalid_handle)
+
+    result = {
+      message: "Not Found",
+      documentation_url: "https://developer.github.com/v3/users/#get-a-single-user"
+    }
+
+    expect(no_data).to eq(result)
   end
 end
