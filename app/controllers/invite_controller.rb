7b8e582e0f880c4d607ed7fbb35c2d9f1db63b3a
nancylee713 <srlee713@gmail.com>
2019-10-15 11:41:46 -0600

    Update create action within InviteController
    Co-authored-by: Fenton Taylor <18686466+fentontaylor@users.noreply.github.com>
    Co-authored-by: Nathan Thomas <47466067+nathangthomas@users.noreply.github.com>

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -7,16 +7,18 @@ class InviteController < ApplicationController
     service = GithubService.new(current_user)
     data = service.get_email(handle)
     email = data[:email]
+    real_name = data[:name]
 
     if data[:message] == 'Not Found'
       flash[:error] = "Sorry, #{handle} is not a valid GitHub handle."
     elsif email.nil?
       flash[:error] = "Sorry, #{handle} does not have a public email."
     else
+      inviter = current_user.first_name + ' ' + current_user.last_name
+      InvitationMailer.invite(real_name, email, inviter).deliver_now
       flash[:success] = "Successfully sent invite!"
     end
-    
+
     redirect_to dashboard_path
   end
-
 end
