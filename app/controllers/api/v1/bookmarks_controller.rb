ed20e2a522138738bd3da8c1011851a136d3a8ed
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-10-16 22:19:16 -0600

    Refactor tutorials controller

--- app/controllers/api/v1/bookmarks_controller.rb
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -2,14 +2,7 @@ class Api::V1::BookmarksController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    user_video = UserVideo.new(user_video_params)
-    if current_user.user_videos.find_by(video_id: user_video.video_id)
-#flash messages will no longer work due to fetch call. Need a solution to this.
-      flash[:error] = "Already in your bookmarks"
-    elsif user_video.save
-      flash[:success] = "Bookmark added to your dashboard!"
-    end
-    redirect_back(fallback_location: root_path)
+    UserVideo.new(user_video_params).save
   end
 
   private
