12e89700142ab873eba86e898c379b272161fd8c
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-10-16 22:47:04 -0600

    Recommit before each to prevent duplicate videos from being bookmarked

--- app/controllers/api/v1/bookmarks_controller.rb
+++ app/controllers/api/v1/bookmarks_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::BookmarksController < ApplicationController
-  
+  skip_before_action :verify_authenticity_token
+
   def create
-    UserVideo.new(user_video_params).save
+    user_video = UserVideo.new(user_video_params)
+    if current_user.user_videos.find_by(video_id: user_video.video_id)
+      flash[:error] = "Already in your bookmarks"
+    elsif user_video.save
+      # flash[:success] = "Bookmark added to your dashboard!"
+    end
+    redirect_back(fallback_location: root_path)
   end
 
   private
